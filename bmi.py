3038bea7ddccdf9b1ddaa896efb274c1f17d4c74
Jing Jie <jingjie.24@ichat.sp.edu.sg>
2025-04-28 15:29:00 +0800

    changes made to bmi file

--- bmi.py
+++ bmi.py
@@ -1,19 +1,25 @@
 def calculate_bmi(height,weight):
     print("Height= " + str(height))
     print("Weight= " + str(weight))
+    bmi = weight/(height*height)
+    return bmi
 
-    bmi= weight/(height*height)
-    
-    print("BMI= " + str(bmi))
+def classify_bmi(bmi_value): 
 
-    if bmi<18.5:
+    if bmi_value<18.5:
         print("Under Weight")
 
-    elif bmi>=18.5 and bmi<=25.0:
+    elif bmi_value>=18.5 and bmi_value<=25.0:
         print("Normal Weight")
 
     else:
         print("Over Weight")
     return
+        
+def main():
+    bmi_output = calculate_bmi(1.8,55)
+    print("BMI: " + str(bmi_output))
+    classify_bmi(bmi_output)
 
-calculate_bmi(weight=57, height=1.73)
+    if __name__ == "__main__":
+        main()
